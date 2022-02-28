7779cbed44ccbe86bc95ce0d687dd87bf8e76020
sklvilya <sklvilyaomsk@gmail.com>
2022-02-28 19:13:21 +0300

    updated file homePage.js_work with favorites. added user but not remove

--- public/homePage.js
+++ public/homePage.js
@@ -35,4 +35,54 @@ money.addMoneyCallback = (data) => {
             money.setMessage(response.success, response.error);
         }
     })
-}
+};
+
+
+money.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response, data);
+            money.setMessage(response.success, `${data.fromAmount} ${data.fromCurrency} конвертированы в ${data.targetCurrency}`);
+        } else {
+            money.setMessage(response.success, response.error);
+        }
+    })
+};
+
+
+money.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, (response) => {
+        console.log(data, response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, `${data.amount} ${data.currency} переведен пользователю № ${data}`);
+        } else {
+            money.setMessage(response.success, response.error);
+        }
+    })
+};
+
+
+let favorites = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+    if (response.success) {
+        favorites.clearTable();
+        favorites.fillTable(response.data);
+        money.updateUsersList(response.data);
+    }
+});
+
+
+favorites.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success) {
+            favorites.clearTable();
+            favorites.fillTable(response.data);
+            money.updateUsersList(response.data);
+            favorites.setMessage(response.success, `${data.name} добавлен в список друзей`);
+        } else {
+            favorites.setMessage(response.success, response.error);
+        }
+    })
+};
